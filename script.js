664a54395eb756f39b895ca335950b77a0f2ea8a
heatherviolet <93415973+heatherviolet@users.noreply.github.com>
2021-12-05 15:15:52 -0800

    html updates buttons

--- script.js
+++ script.js
@@ -1,64 +1,56 @@
-var startButton = document.getElementById("start-btn");
+let btn = document.createElement("button");
+btn.innerHTML = "Start Quiz";
+btn.type = "submit";
+btn.id = "formBtn";
+btn.onclick = function () {
+    alert("Button is clicked");
+};
+document.body.appendChild(btn);
 
-// Start button trigger the first question and next button to display
-startButton.addEventListener("click", startGame);
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion()
-});
+// questions and answers array
 
-// questions and answers for the quiz
-
-    const questions = [
-        question-1 = {
-            textContent: "Commonly used data types do NOT include:",
-            options : [
-                "strings",
-                "booleans",
-                "alerts",
-                "numbers"
-            ],
-            answer : "alerts"
+var questionsArray = [
+        {
+            question: "Commonly used data types do NOT include:",
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers",
+            answer: "alerts"
         },
-        question-2 = {
-            textContent: "The ci=ondition in an if / else statement is enclosed within ______."
-            options : [
-                "quotes",
-                "curly brackets",
-                "parentheses",
-                "square backets"
-            ],
-            answer : "parentheses"
+        {
+            question: "The condition in an if / else statement is enclosed within ______.",
+            1: "quotes",
+            2: "curly brackets",
+            3: "parentheses",
+            4: "square backets",
+            answer: "parentheses"
         },
-        question-3 = {
-            textContent: "Arrays in Javascript can be used to store ______."
-            options: [
-                "numbers and strings",
-                "other arrays",
-                "booleans",
-                "all of the above"
-            ],
-            answer : "all of the above"
+            
+        {
+            question: "Arrays in Javascript can be used to store ______.",
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above",
+            answer: "all of the above"
         },
-        question-4 = {
-            textContent: "Sting values must be enclosed within _____ when being assigned to variables."
-            options : [
-                "commas",
-                "curly brackets",
-                "quotes",
-                "parentheses"               
-            ],
+        {
+            question: "Sting values must be enclosed within _____ when being assigned to variables.",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parentheses",               
             answer: "quotes"
         },
         question_5 = {
-            textContent: "A very useful tool used during development and degugging for printing content to the debugger is:",
-            options : [
-                "JavaScript",
-                "terminal / bash",
-                "for loops",
-                "console log"
-            ],
+            question: "A very useful tool used during development and degugging for printing content to the debugger is:",
+            1: "JavaScript",
+            2: "terminal / bash",
+            3: "for loops",
+            4: "console log",
             answer : "console log"
         }
-    ];
-    init ();
+    ]
+    
+
+    
